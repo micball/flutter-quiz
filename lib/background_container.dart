17f2d3f8beaba5b4fbd23843a576c65faf6b5e18
Michael Ballif <mibal@LAPTOP-H5RVSQNP.localdomain>
2025-08-07 21:53:41 -0700

    first commit

---
+++ lib/background_container.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BackgroundContainer extends StatelessWidget {
+  const BackgroundContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(191, 68, 2, 184),
+            const Color.fromARGB(255, 52, 25, 126),
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/quiz-logo.png', width: 200),
+            SizedBox(height: 30),
+            Text(
+              "Learn Flutter the fun way!",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              // Needs styling to match the more recatngular button style in the sample
+              onPressed: () {},
+              child: Text(
+                "Start Quiz",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
